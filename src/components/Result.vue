36bfcdfa2de16c18d95c2820950f8c63e78249ad
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-12-28 19:18:57 +0100

    first commit

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,10 +1,41 @@
 <template>
+  <div>
+    <table>
+      <tr class="table-head">
+        <th class="name">Name</th>
+        <th class="value">Value</th>
+      </tr>
+      <tr
+        v-for='element in results'
+        :key="element.label"
+      >
+        <td>{{element.label}}</td>
+        <td class="value">
+          {{element.value}}
+        </td>
+      </tr>
+    </table>
 
+    <button
+      v-on:click="handleBackClick"
+      class="btn-back"
+    >
+      Back
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Result"
+  name: "Result",
+  props: {
+    results: Object
+  },
+  methods: {
+    handleBackClick: function() {
+      this.$emit("clearCalculations");
+    }
+  }
 };
 </script>
 
