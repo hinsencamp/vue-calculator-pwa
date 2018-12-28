0f39b39e5e56337a1837091b9c14456d803726bf
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-12-28 19:23:44 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,30 @@
         class="calculator-panel"
         headline="Income Tax Calculator"
       >
+
         <template>
-          <InputForm></InputForm>
+          <transition
+            name="alert-in"
+            mode="out-in"
+            enter-active-class="animated fadeIn"
+            leave-active-class="animated fadeOut"
+          >
+            <InputForm
+              v-if="!resultsCalculated"
+              @submitted="submitted"
+            />
+            <Result
+              v-if="resultsCalculated"
+              @clearCalculations='clearCalculations'
+              :results="calculations"
+            />
+          </transition>
 
         </template>
-      </Panel>
+</Panel>
 
-    </main>
-  </div>
+</main>
+</div>
 </template>
 
 <script>
@@ -27,8 +43,33 @@ export default {
     Panel,
     InputForm,
     Result
+  },
+  data() {
+    return {
+      calculations: {}
+    };
+  },
+  computed: {
+    resultsCalculated: function() {
+      return Object.keys(this.calculations).length !== 0;
+    }
+  },
+  methods: {
+    submitted: function(input) {
+      const calcValues = calcTaxes(input);
+
+      this.calculations = {
+        grossIncome: { label: "Gross Salary", value: calcValues.incomeValue },
+        tax: { label: "Income Tax", value: -calcValues.incomeTax },
+        soli: { label: "Church Tax", value: -calcValues.churchTax },
+        churchTax: { label: "Solidarity Charge", value: -calcValues.soli },
+        netIncome: { label: "Net Salary", value: calcValues.netIncome }
+      };
+    },
+    clearCalculations: function() {
+      this.calculations = "";
+    }
   }
 };
 </script>
-
 <style lang="scss" src="./assets/styles/App.scss"/>
